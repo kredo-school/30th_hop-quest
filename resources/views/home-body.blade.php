c34734d6de2e9fe946ece93c6e3f042f21f87680
GIO-2345-KAZUMA-ONISHI <istudyenglish1223@gmail.com>
2025-03-08 14:16:34 +0900

    I created home-body.blade.php

---
+++ resources/views/home-body.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="card" style="width: 80%;">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h6 class="card-subtitle">Category: </h6>
+                    <h5 class="card-title">XXXX XXXXX</h5>
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
