cd9d734ebc608981d1090a0ad26071e361207995
Tsukasa MIURA <>
2025-03-10 21:10:19 +0800

    created: add-quest.blade.php (deleted: testtest.blade.php)

---
+++ resources/views/quests/add-quest.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+<div class="bg-green">
+@section('content')
+    <h1 class="color-navy poppins-semibold text-center">Create Your Quest</h1>
+    
+    <section id="form1">
+        <form action="" method="post" enctype="multipart/form-data">
+            
+        </form>
+    </section>      
+</div>
+@endsection
