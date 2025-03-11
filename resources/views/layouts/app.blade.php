5ff26293cdd29227a20c06af20c7633f9d451866
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-03-11 21:01:50 +0900

    Adjust logo size, deleted app name (unnecessary)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('HopQuest_logo.png') }}" alt="Logo" style="height: 30px;">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('HopQuest_logo.png') }}" alt="Logo" style="height:50px">                  
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
