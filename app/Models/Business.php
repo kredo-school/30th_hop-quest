31a3ff51fa9c8555a4677f02f8b461a347a3597e
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-03-26 00:57:36 +0900

    Merge branch 'main' into UI250321/Businesses
    # Conflicts:
    #	app/Http/Controllers/HomeController.php
    #	app/Models/Business.php
    #	app/Models/User.php
    #	database/migrations/0001_01_01_000000_create_users_table.php
    #	public/css/navbar-style.css
    #	resources/views/layouts/app.blade.php
    #	routes/web.php

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -5,13 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Business extends Model
 {
     use SoftDeletes;
-
+   
+    //business belongs to one user
     public function user(){
         return $this->belongsTo(User::class);
     }
 
-}
+    //business has many promotions
+    public function promotions(){
+        return $this->hasMany(Promotion::class);
+    }
 
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
+    public function photos(){
+        return $this->hasMany(Photo::class);
+    }
+
+    public function businessLikes(){
+        return $this->hasMany(BusinessLike::class);
+    }
+
+    public function isLiked(){
+        return $this->businessLikes()->where('user_id', Auth::user()->id)->exists();
+    }
+
+}
