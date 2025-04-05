5f4acf4719d183aa32febf5b3801102fa55655ba
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-04-05 17:06:21 +0900

    Adjusted some points.

--- app/Models/BusinessComment.php
+++ app/Models/BusinessComment.php
@@ -22,4 +22,14 @@ class BusinessComment extends Model
     public function BusinessCommentLikes(){
         return $this->hasMany(BusinessCommentLike::class);
     }
+
+    // Review → User（差出人）
+    public function userRelation(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Review → Spot（対象スポット）
+    public function businessRelation(){
+        return $this->belongsTo(Business::class, 'business_id');
+    }
 }
