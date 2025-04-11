2670ec4ac83b49c65147476a8fe1e428505cba07
kenya1104 <k-yamamoto@mitsuwagroup.co.jp>
2025-04-11 22:51:17 +0900

    Modify models about businesses

--- app/Models/BusinessInfoCategory.php
+++ app/Models/BusinessInfoCategory.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusinessInfoCategory extends Model
 {
+    protected $table = 'business_info_category';
+
     public function businessInfos()
     {
         return $this->hasMany(BusinessInfo::class, 'business_info_category_id');
     }
 }
+
