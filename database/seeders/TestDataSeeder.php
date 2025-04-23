424cf8be368cb960c4543d90155f9474916fb87e
kenya1104 <k-yamamoto@mitsuwagroup.co.jp>
2025-04-23 22:14:20 +0900

    add content of business_details to seeder

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -1428,6 +1428,59 @@ class TestDataSeeder extends Seeder
             ['user_id' => 6,  'business_id' => 7],
         ]);
 
+        DB::table('business_info_category')->insert([
+            ['id' => 1, 'name' => 'accessibility'],
+            ['id' => 2, 'name' => 'facilities'],
+            ['id' => 3, 'name' => 'payment option'],
+            ['id' => 4, 'name' => 'smoking policy'],
+            ['id' => 5, 'name' => 'other'],
+        ]);
         
+        DB::table('business_info')->insert([
+            // accessibility (category_id = 1)
+            ['id' => 1, 'business_info_category_id' => 1, 'name' => 'wheelchair'],
+            ['id' => 2, 'business_info_category_id' => 1, 'name' => 'elevator access'],
+            ['id' => 3, 'business_info_category_id' => 1, 'name' => 'accessible parking'],
+            ['id' => 4, 'business_info_category_id' => 1, 'name' => 'braille signage'],
+            ['id' => 5, 'business_info_category_id' => 1, 'name' => 'hearing loop system'],
+            // facilities (category_id = 2)
+            ['id' => 6, 'business_info_category_id' => 2, 'name' => 'free wifi'],
+            ['id' => 7, 'business_info_category_id' => 2, 'name' => 'public restroom'],
+            ['id' => 8, 'business_info_category_id' => 2, 'name' => 'parking available'],
+            ['id' => 9, 'business_info_category_id' => 2, 'name' => 'bicycle parking'],
+            ['id' => 10, 'business_info_category_id' => 2, 'name' => 'changing room'],
+            ['id' => 11, 'business_info_category_id' => 2, 'name' => 'shower facilities'],
+            // payment option (category_id = 3)
+            ['id' => 12, 'business_info_category_id' => 3, 'name' => 'credit card accepted'],
+            ['id' => 13, 'business_info_category_id' => 3, 'name' => 'digital payment'],
+            ['id' => 14, 'business_info_category_id' => 3, 'name' => 'cash only'],
+            ['id' => 15, 'business_info_category_id' => 3, 'name' => 'cash payment accepted'],
+            ['id' => 16, 'business_info_category_id' => 3, 'name' => 'internation payment card'],
+            ['id' => 17, 'business_info_category_id' => 3, 'name' => 'contactless payment'],
+            // smoking policy (category_id = 4)
+            ['id' => 18, 'business_info_category_id' => 4, 'name' => 'completely non-smoking'],
+            ['id' => 19, 'business_info_category_id' => 4, 'name' => 'smoking area available'],
+            ['id' => 20, 'business_info_category_id' => 4, 'name' => 'designated smoking section'],
+            ['id' => 21, 'business_info_category_id' => 4, 'name' => 'outdoor smoking section'],
+            ['id' => 22, 'business_info_category_id' => 4, 'name' => 'smoking permited throughout'],
+        ]);
+
+        DB::table('business_details')->insert([
+            ['id' => 1, 'business_id' => 1, 'business_info_id' => 1, 'is_valid' => '1'],
+            ['id' => 2, 'business_id' => 1, 'business_info_id' => 2, 'is_valid' => '1'],
+            ['id' => 3, 'business_id' => 1, 'business_info_id' => 3, 'is_valid' => '1'],
+            ['id' => 4, 'business_id' => 1, 'business_info_id' => 4, 'is_valid' => '1'],
+            ['id' => 5, 'business_id' => 1, 'business_info_id' => 5, 'is_valid' => '1'],
+            ['id' => 6, 'business_id' => 1, 'business_info_id' => 6, 'is_valid' => '1'],
+            ['id' => 7, 'business_id' => 1, 'business_info_id' => 7, 'is_valid' => '1'],
+            ['id' => 8, 'business_id' => 2, 'business_info_id' => 8, 'is_valid' => '1'],
+            ['id' => 9, 'business_id' => 2, 'business_info_id' => 9, 'is_valid' => '1'],
+            ['id' => 10, 'business_id' => 2, 'business_info_id' => 10, 'is_valid' => '1'],
+            ['id' => 11, 'business_id' => 2, 'business_info_id' => 11, 'is_valid' => '1'],
+            ['id' => 12, 'business_id' => 2, 'business_info_id' => 12, 'is_valid' => '1'],
+            ['id' => 13, 'business_id' => 2, 'business_info_id' => 13, 'is_valid' => '1'],
+            ['id' => 14, 'business_id' => 2, 'business_info_id' => 14, 'is_valid' => '1'],
+            ['id' => 15, 'business_id' => 2, 'business_info_id' => 15, 'is_valid' => '1'],
+        ]);
     }
 }
