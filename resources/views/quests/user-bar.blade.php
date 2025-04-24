df1c2bfd3ed00fa3f7bbc3b23394778d12af531c
Takeshi <nitchy_mura_tkc@yahoo.co.jp>
2025-04-24 20:18:27 +0900

    Modified the post page.

--- resources/views/quests/user-bar.blade.php
+++ resources/views/quests/user-bar.blade.php
@@ -1,58 +1,38 @@
 <div class="row mx-0 px-0 align-items-stretch">
     {{-- RIGHT: Stats --}}
     <div class="col-md-4 px-0 order-1 order-md-2">
-        <div
-            class="bg-white rounded-3 d-flex flex-column justify-content-center border-navy border-2 px-3 m-0 py-1 ms-md-2 mb-3 mb-md-0">
-
+        <div class="bg-white rounded-3 d-flex flex-column justify-content-center border-navy border-2 px-3 m-0 py-1 ms-md-2 mb-3 mb-md-0">
+    
             {{-- Update Date - 右寄せ --}}
             <p class="fs-6 text-secondary text-end mb-0">
                 UPDATE: {{ optional($ques_a->updated_at ?? $quest_a->created_at)->format('Y/m/d') }}
             </p>
-
+    
             {{-- Like / Comment / Views - 横並び＋幅いっぱい --}}
             <div class="d-flex justify-content-center align-items-center w-100 fs-3">
                 {{-- Like --}}
-                <div class="like-button-wrapper position-relative d-inline-block">
-                    <button class="btn-like-toggle border-0 bg-transparent p-0 @guest like-disabled @endguest"
-                        data-quest-id="{{ $quest_a->id }}" data-liked="{{ $quest_a->isLiked() ? '1' : '0' }}">
-                        <i
-                            class="fa{{ $quest_a->isLiked() ? 's' : 'r' }} fa-heart fs-3 like-icon {{ $quest_a->isLiked() ? 'text-danger' : '' }}"></i>
+                <div class="d-flex align-items-center mx-3 mx-md-1 mx-lg-3">
+                    {{-- Like Button --}}
+                    <button 
+                    class="btn-like-toggle border-0 bg-transparent p-0"
+                    data-quest-id="{{ $quest_a->id }}"
+                    data-liked="{{ $quest_a->isLiked() ? '1' : '0' }}">
+                    <i class="fa{{ $quest_a->isLiked() ? 's' : 'r' }} fa-heart fs-3 like-icon {{ $quest_a->isLiked() ? 'text-danger' : '' }}"></i>
                     </button>
 
-                    @guest
-                        <div class="login-tooltip d-none">
-                            Please login to like this quest
-                        </div>
-                    @endguest
+                    {{-- Like Count --}}
+                    <span class="like-count ms-2 poppins-semibold" data-bs-toggle="modal" data-bs-target="#likes-modal-{{ $quest_a->id }}">
+                    {{ $quest_a->likes->count() }}
+                    </span>
 
-                    <a class="like-count poppins-semibold text-decoration-none text-dark" data-bs-toggle="modal"
-                        data-bs-target="#likes-modal-{{ $quest_a->id }}">
-                        {{ $quest_a->likes->count() }}
-                    </a>
                 </div>
-
-    {{-- LEFT: User Info --}}
-    <div class="col-md-8 order-2 order-md-1">
-        <div class="row d-flex flex-wrap align-items-center bg-white rounded-3  h-100">
-            {{-- User Icon --}}
-            <div class="col-auto m-2 align-items-center">
-
-
-                
-                <a href="{{ route('profile.header', $quest_a->user->id) }}" class="text-decoration-none h5 d-flex my-0">
-                    @if ($quest_a->user->avatar)
-                        <img src="{{ $quest_a->user->avatar }}" class="avatar-md rounded-circle ms-0 ms-md-2"
-                            alt="icon">
-                    @else
-                        <i class="fa-solid fa-circle-user text-secondary icon-mmd text-center"></i>
-                    @endif
+    
                 {{-- Comment --}}
-                <a href="#comment-section"
-                    class="d-flex align-items-center mx-md-1 mx-lg-3 text-decoration-none text-dark">
+                <a href="#comment-section" class="d-flex align-items-center mx-3 mx-md-1 mx-lg-3 text-decoration-none text-dark">
                     <i class="far fa-comment icon-sm"></i>
                     <span class="fw-semibold ms-2">{{ $quest_a->questcomments->count() }}</span>
                 </a>
-
+    
                 {{-- Views --}}
                 <div class="d-flex align-items-center mx-3 mx-md-1 mx-lg-3">
                     <i class="fas fa-chart-simple icon-sm"></i>
@@ -68,9 +48,8 @@
             {{-- User Icon --}}
             <div class="col-auto m-2 align-items-center">
                 <a href="{{ route('profile.header', $quest_a->user->id) }}" class="text-decoration-none h5 d-flex my-0">
-                    @if ($quest_a->user->avatar)
-                        <img src="{{ $quest_a->user->avatar }}" class="avatar-md rounded-circle ms-0 ms-md-2"
-                            alt="icon">
+                    @if($quest_a->user->avatar)
+                        <img src="{{ $quest_a->user->avatar }}" class="avatar-md rounded-circle ms-0 ms-md-2" alt="icon">
                     @else
                         <i class="fa-solid fa-circle-user text-secondary icon-md text-center"></i>
                     @endif
@@ -81,16 +60,8 @@
             <div class="col-auto align-items-center">
                 <div class="d-flex align-items-center">
                     {{-- Username --}}
-                    {{-- Username --}}
-                    <a href="{{ route('profile.header', $quest_a->user->id) }}"
-                        class="text-decoration-none h5 d-flex align-items-center my-0 me-3">
-                        <h1 class="username h5 poppins-semibold mb-0 me-2" id="username">{{ $quest_a->user->name }}
-                        </h1>
-
-                        @if ($quest_a->user->official_certification == 3)
-                            <img src="{{ asset('images/logo/official_personal.png') }}" alt="Official Badge"
-                                style="height: 24px;" class="ms-1">
-                        @endif
+                    <a href="{{ route('profile.header', $quest_a->user->id) }}" class="text-decoration-none h5 d-flex my-0 me-3">
+                        <h1 class="username h5 poppins-semibold mb-0" id="username">{{ $quest_a->user->name }}</h1>
                     </a>
 
                     {{-- Follow Button --}}
@@ -103,14 +74,13 @@
                         @if ($authUser->id == 2)
                             <form class="follow-toggle-form mb-0" data-user-id="{{ $owner->id }}">
                                 @csrf
-                                <button type="button"
-                                    class="btn px-3 py-0 {{ $isFollowing ? 'btn-following' : 'btn-follow' }}">
+                                <button type="button" class="btn px-3 py-0 {{ $isFollowing ? 'btn-following' : 'btn-follow' }}">
                                     {{ $isFollowing ? 'Following' : 'Follow' }}
                                 </button>
                             </form>
                         @endif
                     @endauth
-
+                    
                 </div>
             </div>
 
@@ -118,34 +88,30 @@
             {{-- SNS Icons --}}
             <div class="col text-end">
                 <div class="d-flex justify-content-end p-2">
-                    @if (!empty($quest_a->user->instagram))
-                        <a href="https://instagram.com/{{ $quest_a->user->instagram }}" class="text-decoration-none"
-                            target="_blank" rel="noopener">
+                    @if(!empty($quest_a->user->instagram))
+                        <a href="https://instagram.com/{{ $quest_a->user->instagram }}" class="text-decoration-none" target="_blank" rel="noopener">
                             <i class="fa-brands fa-instagram text-dark icon-md mx-1"></i>
                         </a>
                     @endif
-                    @if (!empty($quest_a->user->facebook))
-                        <a href="https://facebook.com/{{ $quest_a->user->facebook }}" class="text-decoration-none"
-                            target="_blank" rel="noopener">
+                    @if(!empty($quest_a->user->facebook))
+                        <a href="https://facebook.com/{{ $quest_a->user->facebook }}" class="text-decoration-none" target="_blank" rel="noopener">
                             <i class="fa-brands fa-facebook text-dark icon-md mx-1"></i>
                         </a>
                     @endif
-                    @if (!empty($quest_a->user->x))
-                        <a href="https://x.com/{{ $quest_a->user->x }}" class="text-decoration-none" target="_blank"
-                            rel="noopener">
+                    @if(!empty($quest_a->user->x))
+                        <a href="https://x.com/{{ $quest_a->user->x }}" class="text-decoration-none" target="_blank" rel="noopener">
                             <i class="fa-brands fa-x-twitter text-dark icon-md mx-1"></i>
                         </a>
                     @endif
-                    @if (!empty($quest_a->user->tiktok))
-                        <a href="https://www.tiktok.com/@{{ $quest_a - > user - > tiktok }}" class="text-decoration-none"
-                            target="_blank" rel="noopener">
+                    @if(!empty($quest_a->user->tiktok))
+                        <a href="https://www.tiktok.com/@{{ $quest_a->user->tiktok }}" class="text-decoration-none" target="_blank" rel="noopener">
                             <i class="fa-brands fa-tiktok text-dark icon-md mx-1"></i>
                         </a>
                     @endif
                 </div>
             </div>
         </div>
-    </div>
+    </div> 
 </div>
 
 @include('quests.modals.quest.likes-modal')
