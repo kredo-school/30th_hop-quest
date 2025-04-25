bb2cb111dbb19f09664070968c003bc82602452a
kenya1104 <k-yamamoto@mitsuwagroup.co.jp>
2025-04-25 22:06:14 +0900

    new migration/seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // ✅ TestDataSeeder を呼び出す
+        $this->call(TestDataSeeder::class);
     }
 }
